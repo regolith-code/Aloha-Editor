f1de005bc2070b4585cdefa6ede9786bc98aac39
JAmadorMarquina <jeremy.amador15@gmail.com>
2023-07-21 15:46:16 -0500

    fix

--- cypress/e2e/highlighteditables-plugin.cy.js
+++ cypress/e2e/highlighteditables-plugin.cy.js
@@ -1,17 +1,14 @@
-describe('list plugin', () => {
+describe('highlighteditables plugin', () => {
     const URL = 'http://localhost:8080/demo/cypress/highlighteditables-plugin.html'
 
     it('Check if content is editable', () => {
         cy.visit(URL)
-      
-        // Verifique que inicialmente el contenido no tiene la clase 'aloha-editable-highlight'
+
         cy.get('#content').should('not.have.class', 'aloha-editable-highlight')
 
-        // Simula el paso del mouse usando realHover() del plugin 'cypress-real-events'
         cy.get('#content').realHover()
         cy.get('#main').realHover()
         
-        // Verifique que ahora el contenido tiene la clase 'aloha-editable-highlight'
         cy.get('#content').should('have.class', 'aloha-editable-highlight')
     })
 })
