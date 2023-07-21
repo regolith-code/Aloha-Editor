6db5b39bc05a2754c364a8a7a3d47efb3cc495ed
JwanCortez93 <jwancortez@gmail.com>
2023-07-21 06:37:25 -0300

    textcolor-plugin testing init

---
+++ cypress/e2e/textcolor-plugin.cy.js
@@ -0,0 +1,10 @@
+describe("CharacterPicker Plugin", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:8080/demo/cypress/textcolor-plugin-testing");
+    cy.get("#main").click();
+  });
+
+  it("opens Aloha Editor UI when clicked", () => {
+    cy.get(".aloha-ui.aloha-ui-toolbar.ui-draggable").should("exist");
+  });
+});
