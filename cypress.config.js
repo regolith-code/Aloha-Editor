d8d0e525d39c91a877d098f2685a3e414ce5608f
JAmadorMarquina <jeremy.amador15@gmail.com>
2023-07-20 20:28:46 -0500

    add test for funcionality

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,6 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'vfjzde',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
