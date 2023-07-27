16f5ec3383f82aa2fc78a81773cc95d6ae092575
Jeremy15a <jeremy.amador15@gmail.com>
2023-07-26 23:59:04 -0400

    xhtml

--- cypress/e2e/dom-to-xhtml-plugin.cy.js
+++ cypress/e2e/dom-to-xhtml-plugin.cy.js
@@ -1,10 +1,16 @@
 describe('dom-to-xhtml plugin', () => {
 
-    const URL = 'http://localhost:8080/demo/cypress/dom-to-xhtml-plugin.html'
+    const URL = 'http://localhost:8080/demo/modules/dom-to-xhtml-plugin.html'
 
     it('Check ', () => {
         cy.visit(URL)
       
         cy.get('#content').click()
+
+        cy.window().then((win) => {
+            const editable = win.Aloha.getEditableById('content');
+            const xhtmlContent = editable.getContents();
+            expect(xhtmlContent).to.match('/<p>DOM-TO-XHTML PLUGIN<\/p>/');
+        });
     })
 })
