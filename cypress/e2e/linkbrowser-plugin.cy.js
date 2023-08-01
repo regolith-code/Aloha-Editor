cf5d2f6164c4f3a4da9417f169b75e30ff8e4744
Jeremy15a <jeremy.amador15@gmail.com>
2023-08-01 00:54:17 -0400

    test for linkbrowser plugin

--- cypress/e2e/linkbrowser-plugin.cy.js
+++ cypress/e2e/linkbrowser-plugin.cy.js
@@ -1,6 +1,6 @@
 describe('linkbrowser plugin', () => {
 
-    const URL = 'http://localhost:8080/demo/modules/link.html'
+    const URL = 'http://localhost:8080/demo/modules/linkbrowser.html'
 
     it('Verify the existence of the "Link" button in Format tab', () => {
         cy.visit(URL)
@@ -36,5 +36,24 @@ describe('linkbrowser plugin', () => {
 
         cy.get('#tab-ui-container-3 button[title="Remove Link"]').should('exist').click()
         cy.get('#content p a').should('not.exist');
+    });
+
+    it('Verify the existence of the "Insert Link" button in tab-ui-container-3', () => {
+        cy.visit(URL)
+      
+        cy.get('#content').click()
+        cy.get('#ui-id-1').click()
+        cy.get('#tab-ui-container-1').contains('button', 'Insert Link').click()
+
+        cy.get('#ui-id-3').should('exist').click()
+        cy.get('#tab-ui-container-3').should('be.visible')
+        cy.get('#tab-ui-container-3').contains('button', 'Insert Link').should('exist').should('be.visible').click()
+
+        cy.get('div.repository-browser-modal-window.ui-resizable.aloha-dialog.ui-draggable.ui-resizable-autohide')
+        .should('exist')
+        .should('be.visible')
+        .within(() => {
+            cy.get('a').contains('link-languages').should('exist').should('be.visible')
+        });
     });
 })
