470c4f53fd0573066ad6abf44ac43778d6096032
JwanCortez93 <jwancortez@gmail.com>
2023-09-04 18:08:46 -0300

    fix-load-wrongly-changed-to-on

--- src/lib/vendor/jquery-ui-1.13.2.js
+++ src/lib/vendor/jquery-ui-1.13.2.js
@@ -17770,7 +17770,7 @@ define('jqueryui', ['jquery'], function (jQuery) {
 				this._refresh();
 
 				if ( this.active.length ) {
-					this.on("load", options.active );
+					this.load(options.active );
 				}
 			},
 
@@ -18290,7 +18290,7 @@ define('jqueryui', ['jquery'], function (jQuery) {
 				}
 
 				if ( toShow.length ) {
-					this.on("load", this.tabs.index( tab ), event );
+					this.load( this.tabs.index( tab ), event );
 				}
 				this._toggle( event, eventData );
 			},
